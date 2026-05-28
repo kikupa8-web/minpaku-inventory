c75f00fbabafae9da85d97b52f53efa96b65de3f
kikupa8-web <kikupa8@gmail.com>
2026-05-28 14:23:49 +0900

    Revert "アプリ名を「AssistBnB」に変更"
    This reverts commit a11108a0d7c2c094d5ce406ed137a383a56bb874.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var AppConfig = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbzbzRM0xgMSIE11IeBUgi8jeuuCqQIgUixv0ceVunr4B9zvki6GcFobSxDYU4Mqm4qm/exec',
   GOOGLE_CLIENT_ID: '449521724423-6qlv4uommuankajslp6amp9r14daaogv.apps.googleusercontent.com',
-  APP_NAME: 'AssistBnB',
+  APP_NAME: '民泊在庫管理',
   VERSION: '1.0.0'
 };
